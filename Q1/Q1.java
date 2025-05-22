17b70ec7d531cc9dd27a8a6074c20099dfef5076
ExplodedChromebook <davids756@nycstudents.net>
2025-05-21 21:14:19 -0400

    Update Q1.java

--- Q1/Q1.java
+++ Q1/Q1.java
@@ -1,1 +1,30 @@
+class DogWalkCompany{
+    //Part A
+    public int walkDogs(int hour){
+        int numDogs = numAvailableDogs(hour);
+        int max = maxDogs;
+        if(max<numDogs){
+            company.updateDogs(hour, max);
+            return max;
+        }
+        company.updateDogs(hour, numDogs);
+        return numDogs;
 
+    }
+    //Part B
+    public int dogWalkedShift(int startHour,int endHours){
+        int cash = 0;
+        int x = startHour;
+        while(x<=endHours){
+            int y = walkDogs(x);
+            dogWalked+=y
+            if((x>=9 && x<=17)||(y==maxDogs)){
+                cash+=3;
+            }else{
+                cash+=(dogWalked*5);
+            }
+            x++;
+        }
+        return cash;
+    }
+}
