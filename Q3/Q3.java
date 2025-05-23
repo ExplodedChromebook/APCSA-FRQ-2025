59125b33f484f8c6bb00381e16c4fb2d93da79db
ExplodedChromebook <davids756@nycstudents.net>
2025-05-22 22:35:07 -0400

    Update Q3.java

--- Q3/Q3.java
+++ Q3/Q3.java
@@ -1,1 +1,11 @@
-
+Class Round{
+  private String name;
+  private int rank;
+  public Round(String[] list){
+    int rank = 1;
+    for(int x:name){
+      competitorList.add(competitor(x,rank));
+      rank++;
+    }
+  }
+}
