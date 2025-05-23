e310b0c574c9b33498bbeaf92f3763c7cee092d6
ExplodedChromebook <davids756@nycstudents.net>
2025-05-22 22:52:52 -0400

    Update Q3.java

--- Q3/Q3.java
+++ Q3/Q3.java
@@ -8,4 +8,17 @@ Class Round{
       rank++;
     }
   }
+  public Array<Match> buildMatch(){
+    ArrayList<Match> matches = ArrayList<Match>();
+    int index = 0;
+    if(CompetitorList.size()%2==1){
+      index = 1;
+    }
+    int x =0;
+    while(x<((CompetitorList.size()-index)/2)){
+      matches.add( Match(competitorList.get(x),competitorList.get(competitorList.size()-1-(x-index))));
+      x++;
+    }
+    return matches;
+  }
 }
