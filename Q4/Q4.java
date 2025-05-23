5ed0ca21463ce97a30d2b6a0741a6476ba823e86
ExplodedChromebook <davids756@nycstudents.net>
2025-05-22 23:10:42 -0400

    Update Q4.java

--- Q4/Q4.java
+++ Q4/Q4.java
@@ -1,1 +1,13 @@
-
+public class sumOrSameGame{
+  private int [][] puzzle;
+  public sumOrSameGame(int rows,int cols){
+    puzzle = new int[rows][ccols];
+    for(int x = 0; x<puzzle.length;x++){
+      for(int y = 0; y<puzzle[0].length;y++;)
+      puzzle[x][y]=(int)(Math.random()*9-1+1)+1;
+    }
+  }
+  public boolean clearPair(int row,int cols){
+    
+  }
+}
